32f11a70c9223192d31f794a14e24d87638d74fb
HBB <b845548@gmail.com>
2017-05-02 16:48:33 +0200

    works

---
+++ PTAMM/AliceScript.cc
@@ -0,0 +1,107 @@
+#include"AliceScript.h"
+
+AliceScript::AliceScript(void){
+    state=STATE_INIT;    
+    system("rm test.wav");   
+    system("rm result.tmp");   
+    system("rm finish.tmp");   
+    system("rm question.tmp");   
+
+}
+std::string AliceScript::get_answer(void){
+    return mAnswer;
+}
+
+int AliceScript::get_state(void){
+    return state;
+}
+void AliceScript::set_state(int state){
+    this->state=state;
+}
+
+void AliceScript::gen_answer(std::string question){
+    set_state(STATE_MAKING);
+    FILE* f;
+    f=fopen("question.tmp","w");
+    fputs((question+"\nq").c_str(),f);
+    fclose(f);
+    std::string script = std::string("(java -cp out/production/Ab Main bot=super action=chat trace=false < question.tmp | grep \"Robot\" > result.tmp; exit ) &");
+    system(script.c_str());   
+
+}
+
+
+void AliceScript::finish_make(void){
+FILE* f;
+int count;
+f=fopen("result.tmp","r");
+if(f==NULL)
+    return;
+for(count=0;fgetc(f)!=EOF;count++);
+fclose(f);
+if(count<14)
+    return;
+
+set_state(STATE_STARTSPEAK);
+
+}
+
+void AliceScript::gen_speak(void){
+FILE* f;
+char buff[100];
+f=fopen("result.tmp","r");
+if(f==NULL)
+    return;
+if(fgets(buff,100,f)==NULL)
+    return;
+
+std::string parse = std::string (buff); 
+std::string answer = parse.substr (14,parse.size()-14);
+mAnswer=answer;
+std::string prefix = std::string("((pico2wave -w test.wav \"");
+std::string suffix = std::string("\"; aplay test.wav ; echo a > finish.tmp ); exit) &");
+
+system((prefix+answer+suffix).c_str());
+
+set_state(STATE_SPEAKING);
+
+}
+
+void AliceScript::finish_speak(void){
+FILE* f;
+f=fopen("finish.tmp","r");
+if(f==NULL)
+    return;
+system("rm finish.tmp");
+set_state(STATE_INIT);
+
+}
+/*
+int main(){
+AliceScript as;
+std::string question=std::string("hello");
+int stop=2;
+while(stop){
+std::cout<<as.get_state();
+switch(as.get_state()){
+    case STATE_INIT:
+        stop--;
+        as.gen_answer(question);
+    break;
+    case STATE_MAKING:
+        as.finish_make();
+    break;
+    case STATE_STARTSPEAK:
+        as.gen_speak();
+    break;
+    case STATE_SPEAKING:
+        as.finish_speak();
+    break;
+    default:
+    break;
+    
+}
+
+}
+}
+*/
---
+++ PTAMM/AliceScript.h
@@ -0,0 +1,35 @@
+#include<cstdlib>
+#include<cstdio>
+#include<iostream>
+#include<string>
+
+
+
+
+enum{
+    STATE_INIT,
+    STATE_START,
+    STATE_MAKING,
+    STATE_STARTSPEAK,
+    STATE_SPEAKING,
+};
+
+
+
+
+class AliceScript{
+	int state;
+	std::string mAnswer;
+  public:
+    AliceScript(void);
+    int get_state(void);
+
+	std::string get_answer(void);
+    void set_state(int);
+    void gen_answer(std::string);
+    void gen_speak();
+    void finish_make(void);
+    void finish_speak(void);
+    
+};
+
